156195301d768a8b631add1485688f3c694a823b
Daniel Masek <daniel.masek@monster.com>
2021-04-10 10:50:07 +0200

    Spring application initialization

---
+++ src/main/java/cz/masci/flightevents/FlightEventsAppApplication.java
@@ -0,0 +1,13 @@
+package cz.masci.flightevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightEventsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightEventsAppApplication.class, args);
+	}
+
+}
